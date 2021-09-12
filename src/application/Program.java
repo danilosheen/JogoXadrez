c06d98db1de78f6967c0a767e1ef73b3cbcf1f03
danilosheen <c.danilo.f.silva@gmail.com>
2021-09-11 22:14:56 -0300

    comentário no main

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,18 @@ package application;
 
 import chess.ChessMatch;
 
+//Programa principal
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
+		
+		for (int i=0; i<10; i++) {
+			System.out.println("AA");
+		}
 
 	}
 
